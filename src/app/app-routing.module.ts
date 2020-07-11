ece01e547b2c0cba46d0221302e0d3305e449551
Franck Ngami <ufrank238@yahoo.fr>
2020-07-11 21:16:40 +0200

    First Commits

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,34 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'chat',
+    loadChildren: () => import('./chat/chat.module').then( m => m.ChatPageModule)
+  },
+  {
+    path: 'consultation',
+    loadChildren: () => import('./consultation/consultation.module').then( m => m.ConsultationPageModule)
+  },
+  {
+    path: 'history',
+    loadChildren: () => import('./history/history.module').then( m => m.HistoryPageModule)
+  },
+  {
+    path: 'information',
+    loadChildren: () => import('./information/information.module').then( m => m.InformationPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'menu',
+    loadChildren: () => import('./menu/menu.module').then( m => m.MenuPageModule)
+  },
+  {
+    path: 'who-ami',
+    loadChildren: () => import('./who-ami/who-ami.module').then( m => m.WhoAmiPageModule)
+  },
 ];
 
 @NgModule({
---
+++ src/app/who-ami/who-ami.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-who-ami',
+  templateUrl: './who-ami.page.html',
+  styleUrls: ['./who-ami.page.scss'],
+})
+export class WhoAmiPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
