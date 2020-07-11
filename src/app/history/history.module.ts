a00564314139267b54c7427a6c13b77ad362a3e0
Javianer <berger-25@web.de>
2020-07-11 22:03:55 +0200

    history.module.ts beendet

--- src/app/history/history.module.ts
+++ src/app/history/history.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
+import { NgCalendarModule  } from 'ionic2-calendar';
 import { IonicModule } from '@ionic/angular';
-
+import {CdkStepperModule} from '@angular/cdk/stepper'; 
 import { HistoryPageRoutingModule } from './history-routing.module';
-
+import {MatStepperModule, MatInputModule, MatFormFieldModule} from '@angular/material';
 import { HistoryPage } from './history.page';
 
 @NgModule({
@@ -15,6 +15,7 @@ import { HistoryPage } from './history.page';
     MatInputModule,
     FormsModule,
     MatFormFieldModule,
+    MatStepperModule,
     IonicModule,
     CdkStepperModule,
     HistoryPageRoutingModule
