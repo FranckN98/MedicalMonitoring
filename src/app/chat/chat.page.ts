515f0a5a96f62c62f9704870c34890d29c863089
Franck Ngami <ufrank238@yahoo.fr>
2020-07-12 09:25:18 +0200

    Chat Schreiben einer Nachricht in der DatenBank

--- src/app/chat/chat.page.ts
+++ src/app/chat/chat.page.ts
@@ -1,15 +1,52 @@
+import { AngularFireAuth } from '@angular/fire/auth';
+import { GeneralService } from './../service/general.service';
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.page.html',
-  styleUrls: ['./chat.page.scss'],
+  styleUrls: ['./chat.page.css'],
 })
 export class ChatPage implements OnInit {
 
-  constructor() { }
+ 
+  public messages: any = [];
+  doctors: any = []
+  partnerSelect = false;
+  selectedDoctor = { 
+    name:"",
+    doctorId: "",
+    startDate: "",
+    rating: "",
+    town: ""
+  };
+  messageText : any; 
+  userId  ;
+  
+  constructor(public afDB: AngularFireDatabase,public afAuth: AngularFireAuth, public generalService: GeneralService) { 
+
+    this.userId = this.generalService.userId;
+    this.getMessages();
+    this.getDoctors();
+  }
 
   ngOnInit() {
+  }
+
+
+  sendMessage()
+  {
+ 
+    console.log(this.selectedDoctor)
+    this.afDB.list('Messages/').push({
+      userId: this.userId,
+      text: this.messageText,
+      date:new Date().toString(),
+      partnerId : this.selectedDoctor.doctorId
+    })
+
+    this.messageText ='';
   }
 
 }
