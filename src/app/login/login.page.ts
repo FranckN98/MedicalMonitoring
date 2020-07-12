3b68a31364504fad2a279f963e89bad3b2f76588
unknown <chrit.anago@gmail.com>
2020-07-12 12:08:49 +0200

    variable init

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,13 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AngularFireAuth } from '@angular/fire/auth';
+
+import { GeneralService } from '../service/general.service';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  styleUrls: ['./login.page.css'],
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  doctor: boolean;
+  connected : boolean;
+  constructor(public generalService:GeneralService) 
+  { 
+
+  }
+
 
   ngOnInit() {
   }
