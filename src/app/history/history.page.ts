329cebea9de0517996e54d2aad312156e70330b2
Javianer <berger-25@web.de>
2020-07-12 09:57:00 +0200

    history.page.ts import hinzugefuegt

--- src/app/history/history.page.ts
+++ src/app/history/history.page.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { GeneralService } from './../service/general.service';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { Component, OnInit , ViewChild } from '@angular/core';
+import {AlertController, IonicSafeString} from '@ionic/angular'
+import { CalendarComponent } from 'ionic2-calendar/calendar';
+
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+import { stringify } from 'querystring';
+registerLocaleData(localeFr, 'fr');
 
 @Component({
   selector: 'app-history',
