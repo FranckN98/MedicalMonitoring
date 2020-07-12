aa5199f9bf15bf4740dc1380d967b3afd1dd695a
Franck Ngami <ufrank238@yahoo.fr>
2020-07-12 09:41:02 +0200

    Einrichtung von Routen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,44 +1,19 @@
+import { InformationPage } from './information/information.page';
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'chat',
-    loadChildren: () => import('./chat/chat.module').then( m => m.ChatPageModule)
-  },
-  {
-    path: 'consultation',
-    loadChildren: () => import('./consultation/consultation.module').then( m => m.ConsultationPageModule)
-  },
-  {
-    path: 'history',
-    loadChildren: () => import('./history/history.module').then( m => m.HistoryPageModule)
-  },
-  {
-    path: 'information',
-    loadChildren: () => import('./information/information.module').then( m => m.InformationPageModule)
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-  },
-  {
-    path: 'menu',
-    loadChildren: () => import('./menu/menu.module').then( m => m.MenuPageModule)
-  },
-  {
-    path: 'who-ami',
-    loadChildren: () => import('./who-ami/who-ami.module').then( m => m.WhoAmiPageModule)
-  },
+  { path: '', redirectTo: 'who', pathMatch: 'full' },
+  { path: 'menu', loadChildren:  () => import('./menu/menu.module').then( m => m.MenuPageModule)},
+  { path: 'info', loadChildren:  () => import('./information/information.module').then( m => m.InformationPageModule)},
+  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'history', loadChildren: () => import('./history/history.module').then( m => m.HistoryPageModule)},
+  { path: 'consult', loadChildren: () => import('./consultation/consultation.module').then( m => m.ConsultationPageModule)},
+  { path: 'who', loadChildren: () => import('./who-ami/who-ami.module').then( m => m.WhoAmiPageModule)},
+  { path: 'login',loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule) },
+  { path: 'chat',  loadChildren: () => import('./chat/chat.module').then( m => m.ChatPageModule)},
+
+ 
 ];
 
 @NgModule({
@@ -48,3 +23,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+ 
