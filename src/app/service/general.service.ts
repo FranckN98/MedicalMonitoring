aabb3f1738dd8e507723839faecf265dc1fba69c
Javianer <berger-25@web.de>
2020-07-12 22:16:13 +0200

    general.service.ts update

--- src/app/service/general.service.ts
+++ src/app/service/general.service.ts
@@ -34,5 +34,52 @@ export class GeneralService {
     })
   }
 
+  selectUserType(i:number)
+  {
+    if(i == 1) // 1 = Not a Patient
+    {
+      this.doctor = true; 
+      this.patient = false; 
+    }
+    else
+    { 
+      this.doctor = false; 
+      this.patient = true; 
+    }
+  }
+
+  login(email:string, password:string)
+  {
+    this.afAuth.signInWithEmailAndPassword(email,password)
+    .then(value => {
+      
+      if(this.doctor)
+      {
+        this.navCtrl.navigateRoot('/home');
+        this.connected = true;
+      }
+      else
+      {
+        this.navCtrl.navigateRoot('/menu');
+        this.connected = true;
+      }
+    })
+    .catch(err => {
+      console.log('Something went wrong:',err.message);
+    });
+    this.afAuth.authState.subscribe(auth=>{
+      if(!auth)
+      {
+        this.connected = false;
+      }
+      else
+      {
+        this.connected = true;
+        this.userId = auth.uid; 
+        
+      }
+    })
+   
+  }
 
 }
