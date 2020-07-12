9367f2d47c4bc4ff8f6caf90406b1157d064c30d
aquilonallel <aquilon.ah@gmail.com>
2020-07-12 11:54:07 +0200

    variable init

--- src/app/consultation/consultation.page.ts
+++ src/app/consultation/consultation.page.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-consultation',
   templateUrl: './consultation.page.html',
-  styleUrls: ['./consultation.page.scss'],
+  styleUrls: ['./consultation.page.css'],
 })
 export class ConsultationPage implements OnInit {
 
-  constructor() { }
+  backButton: boolean = true;
+  step1: boolean = true;
+  step2: boolean;
+  step3: boolean;
+  constructor() {
+
+    if (this.step1) this.backButton = true;
+  }
 
   ngOnInit() {
   }
-
 }
